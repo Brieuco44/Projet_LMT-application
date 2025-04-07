c008979213a02d5997b107eeda8a8bf2d37fa44f
Brieuc C. <brieuc.courapie@free.fr>
2025-04-07 23:10:53 +0200

    Ajout du menu admin

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\UtilisateurRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class IndexController extends AbstractController
 {
@@ -15,6 +16,7 @@ final class IndexController extends AbstractController
     }
 
     #[Route('/', name: 'app_index')]
+    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
         return $this->render('index/index.html.twig', [
